2b389d33fc12b0e03cb9f3da83b1b6abd85a18bf
milinD537 <milindpithadia22@gmail.com>
2024-08-11 22:32:58 +0530

    pages commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	basePath: "/portfolio-marknox",
+	output: "export",
+	reactStrictMode: true,
+}
 
-export default nextConfig;
+export default nextConfig
